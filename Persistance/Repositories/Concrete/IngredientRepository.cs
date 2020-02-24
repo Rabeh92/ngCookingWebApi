1378956ea6ebaccf3905557eb2fb2d72fe05bd05
rabeh.mahmoudi <rabeh.mahmoudi@hotmail.fr>
2020-02-24 14:32:38 +0100

    Implement IIngredientRepository interface

--- Persistance/Repositories/Concrete/IngredientRepository.cs
+++ Persistance/Repositories/Concrete/IngredientRepository.cs
@@ -2,6 +2,7 @@
 using ngCookingWebApi.Persistance.Repositories.Abstract;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -9,29 +10,46 @@ namespace ngCookingWebApi.Persistance.Repositories.Concrete
 {
     public class IngredientRepository : IIngredientRepository
     {
+        private readonly ApplicationDbContext _context;
+        public IngredientRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void AddNewIngredient(Ingredient ingredient)
         {
-            throw new NotImplementedException();
+            _context.Ingredients.Add(ingredient);
+            
         }
 
         public void DeleteIngredient(int id)
         {
-            throw new NotImplementedException();
+
+            _context.Ingredients.Remove(_context.Ingredients.Single(i => i.Id == id));
         }
 
         public IEnumerable<Ingredient> GetAlIngredient()
         {
-            throw new NotImplementedException();
+            return _context.Ingredients.ToList();
         }
 
         public Ingredient GetIngredient(int id)
         {
-            throw new NotImplementedException();
+            return _context.Ingredients.FirstOrDefault(i => i.Id == id);
         }
 
         public void UpdateUpdateIngredient(int id, Ingredient ingredient)
         {
-            throw new NotImplementedException();
+            var ingredientInDb = _context.Ingredients.FirstOrDefault(i => i.Id == id);
+            // set original entity state to detached
+            _context.Entry(ingredientInDb).State = EntityState.Detached;
+
+            // attach & save
+            _context.Ingredients.Attach(ingredient);
+
+            // set the updated entity state to modified, so it gets updated.
+            _context.Entry(ingredient).State = EntityState.Modified;
+
+
         }
     }
 }

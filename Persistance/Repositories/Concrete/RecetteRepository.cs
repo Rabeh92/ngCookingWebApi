4c252cb7f6b5eb0d2248108a61ce2b7cd792f35a
rabeh.mahmoudi <rabeh.mahmoudi@hotmail.fr>
2020-02-24 11:11:43 +0100

    Create RecetteRepository interface

---
+++ Persistance/Repositories/Concrete/RecetteRepository.cs
@@ -0,0 +1,37 @@
+﻿using ngCookingWebApi.Models;
+using ngCookingWebApi.Persistance.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ngCookingWebApi.Persistance.Repositories.Concrete
+{
+    public class RecetteRepository : IRecetteRepository
+    {
+        public void AddNewRecette(Recette recette)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRecette(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Recette> GetAllRecette()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Recette GetRecette(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRecette(Recette recette, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

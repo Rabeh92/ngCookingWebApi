cdb9542ce2694811cc0a868edd78be43e6331a01
rabeh.mahmoudi <rabeh.mahmoudi@hotmail.fr>
2020-02-24 11:01:01 +0100

    Delete IRepository interface

--- Persistance/IUnitOfWork.cs
+++ Persistance/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ngCookingWebApi.Persistance.Repositories.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace ngCookingWebApi.Persistance
 {
     interface IUnitOfWork
     {
+       
     }
 }
---
+++ Persistance/Repositories/Concrete/IngredientRepository.cs
@@ -0,0 +1,14 @@
+﻿using ngCookingWebApi.Models;
+using ngCookingWebApi.Persistance.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ngCookingWebApi.Persistance.Repositories.Concrete
+{
+    public class IngredientRepository 
+    {
+        
+    }
+}

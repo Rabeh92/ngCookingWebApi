f5e3728752056e98cd37fab21b83b4b2d26eb438
rabeh.mahmoudi <rabeh.mahmoudi@hotmail.fr>
2020-02-24 11:19:16 +0100

    Create CommentRepository class

---
+++ Persistance/Repositories/Concrete/CommentRepository.cs
@@ -0,0 +1,27 @@
+﻿using ngCookingWebApi.Models;
+using ngCookingWebApi.Persistance.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ngCookingWebApi.Persistance.Repositories.Concrete
+{
+    public class CommentRepository : ICommentRepository
+    {
+        public void AddCommentToRecette(Comment comment, int idRecette)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Comment> GetCommentByRecette(int idRecette)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetMarktByRecette(int idRecette)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

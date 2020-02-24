2269d913404575fa010afc53ceb89aad5ff99cc4
rabeh.mahmoudi <rabeh.mahmoudi@hotmail.fr>
2020-02-24 11:10:19 +0100

    Create IRecetteRepositoryInterface

---
+++ Persistance/Repositories/Abstract/IRecetteRepository.cs
@@ -0,0 +1,18 @@
+﻿using ngCookingWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ngCookingWebApi.Persistance.Repositories.Abstract
+{
+    interface IRecetteRepository
+    {
+        IEnumerable<Recette> GetAllRecette();
+        Recette GetRecette(int id);
+        void AddNewRecette(Recette recette);
+        void UpdateRecette(Recette recette, int id);
+        void DeleteRecette(int id);
+    }
+}

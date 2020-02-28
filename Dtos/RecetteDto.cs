6517e1b0ea1f9444183fdeb12e38f2a38b334a8a
rabeh.mahmoudi <rabeh.mahmoudi@hotmail.fr>
2020-02-28 15:31:36 +0100

    Create RecetteDto class

---
+++ Dtos/RecetteDto.cs
@@ -0,0 +1,25 @@
+﻿using ngCookingWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ngCookingWebApi.Dtos
+{
+    public class RecetteDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long CreationDate { get; set; }
+        public ApplicationUser User { get; set; }
+        public string UserId { get; set; }
+        public bool IsAvailable { get; set; }
+        public CategorieRecette Categorie { get; set; }
+        public int CategorieId { get; set; }
+        public string Picture { get; set; }
+        public int Calorie { get; set; }
+        //public ICollection<Ingredient> Ingredients { get; set; }
+        public string Preparation { get; set; }
+        //public ICollection<Comment> Comments { get; set; }
+    }
+}

a26176f0eb90a6a5612a4df4450d8517d6ee2d79
Rabeh <rabeh.mahmoudi@gmail.com>
2020-03-01 11:32:48 +0100

    Create CommentDto class

---
+++ Dtos/CommentDto.cs
@@ -0,0 +1,28 @@
+﻿using ngCookingWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ngCookingWebApi.Dtos
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public int Mark { get; set; }
+
+        public Recette Recette { get; set; }
+        public int RecetteId { get; set; }
+        public ApplicationUser User { get; set; }
+        public string UserId { get; set; }
+    }
+}
